7c9a09d1ef5621ff77b5760e178908bdc7fb6e0e
m.niedbal <m.niedbal@pixel.com.pl>
2022-04-20 15:04:06 +0200

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -22,15 +22,18 @@ public class Main extends Application {
     private double x;
     private double y;
 
-    private int clickCount = 0;
+    private int clickCount1 = 0;
+    private int clickCount2 = 0;
 
     @Override
     public void start(Stage primaryStage) {
         initButtons(primaryStage);
         root = new VBox(dragButton, additionalButton, exitButton);
-        Scene scene = new Scene(root);
         root.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
-        root.setStyle("-fx-border-color: #66666680;-fx-border-width:5px;-fx-border-radius:5px");
+        root.setStyle("-fx-border-color: #666666aa;-fx-border-width:50px;-fx-border-radius:5px");
+        initDragListener(primaryStage);
+
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         scene.setFill(Color.TRANSPARENT);
@@ -40,29 +43,33 @@ public class Main extends Application {
     }
 
     private void initButtons(Stage primaryStage) {
-        dragButton = getButton("Drag this button");
-        additionalButton = getButton("Click counter");
+        dragButton = getButton("Click counter 1");
+        additionalButton = getButton("Click counter 2");
         exitButton = getButton("EXIT");
-        initDragListener(primaryStage, dragButton);
         exitButton.setOnAction(event -> {
             Platform.exit();
             System.exit(0);
         });
-        additionalButton.setOnAction(event -> {
-            clickCount++;
-            additionalButton.setText("Clicked " + clickCount + " times");
+        dragButton.setOnAction(event -> {
+            clickCount1++;
+            dragButton.setText("Clicked " + clickCount1 + " times");
         });
+        additionalButton.setOnAction(event -> {
+            clickCount2++;
+            additionalButton.setText("Clicked " + clickCount2 + " times");
+        });
+
     }
 
-    private void initDragListener(Stage primaryStage, Button button) {
-        button.setCursor(Cursor.MOVE);
+    private void initDragListener(Stage primaryStage) {
+        root.setCursor(Cursor.MOVE);
 
-        button.setOnMousePressed(event -> {
+        root.setOnMousePressed(event -> {
             x = event.getScreenX();
             y = event.getScreenY();
         });
 
-        button.setOnMouseDragged(event -> {
+        root.setOnMouseDragged(event -> {
             x -= event.getScreenX();
             y -= event.getScreenY();
             primaryStage.setX(primaryStage.getX() - x);
@@ -74,11 +81,16 @@ public class Main extends Application {
 
     private Button getButton(String name) {
         Button button = new Button(name);
-        button.setStyle("-fx-border-color: #999999;-fx-background-color: #99999933;-fx-text-fill:#ffffff;-fx-font-size:50px;-fx-border-radius:4px");
+        button.setCursor(Cursor.HAND);
+
+        button.setStyle("-fx-border-color: #999999;-fx-background-color: #99999933;-fx-text-fill:#ffffff;-fx-font-size:50px;-fx-border-radius:4px;-fx-background-radius:4px");
         button.setPrefWidth(800);
         button.setPrefHeight(100);
         DropShadow dropShadow = new DropShadow();
-        dropShadow.setHeight(10);
+        dropShadow.setHeight(5);
+        dropShadow.setOffsetX(-2);
+        dropShadow.setOffsetY(-1);
+        dropShadow.setSpread(0.3);
 
         ColorAdjust colorAdjust = new ColorAdjust();
 
